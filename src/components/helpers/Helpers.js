80e3a5e3eb10b3ed8e5f599aab383fcde92268fe
yotsaphonSutweha <komsan_58@hotmail.com>
2020-04-18 19:20:04 +0100

    use session inside the helpers file

--- src/components/helpers/Helpers.js
+++ src/components/helpers/Helpers.js
@@ -2,7 +2,7 @@ import Cookies  from 'js-cookie';
 
 const helpers = {
     'checkIfCookiesExists' : function() {
-        if(Cookies.get('hddt')) {
+        if(Cookies.get('session')) {
             return true;
         }
         return false;
